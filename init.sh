e2bfaae52727a3d9d1607c4fd271dc15e109c924
Istvan Cebrian <istvan.cebrian@gmail.com>
2019-11-16 19:03:33 +0000

    v2

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Starting Apache ..."
-#service apache2 start
-/usr/sbin/apache2ctl -D FOREGROUND
+echo "Start PHP-FPM ..."
+service php7.2-fpm start
+
+echo "Starting nginx ..."
+service nginx start
+
+echo "Starting Varnish ..."
+service varnish start
+
+#/usr/sbin/apache2ctl -D FOREGROUND
+#/usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
+
+echo "Tailing nginx error log ..."
+/usr/bin/tail -f /var/log/nginx/error.log
